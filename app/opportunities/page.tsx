ccdd54e47aef23d6d6bfcc83df68b546fcb40a30
Aryan Rajpal <rajpalaryan8@gmail.com>
2026-02-21 15:16:46 -0600

    Clear out default homepage and add routes to profile and opportunities

---
+++ app/opportunities/page.tsx
@@ -0,0 +1,7 @@
+export default function Opportunities() {
+    return (
+        <main className="flex min-h-screen justify-center items-center">
+            <h1>Post Opportunity</h1>
+        </main>
+    )
+}
---
+++ app/profile/page.tsx
@@ -0,0 +1,8 @@
+export default function ProfilePage() {
+    return (
+        <main className="flex min-h-screen justify-center items-center">
+            <h1>Create Profile</h1>
+        </main>
+    );
+}
+

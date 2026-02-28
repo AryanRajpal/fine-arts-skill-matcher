a2562426d41a24406ba19afe672ca51a046163b5
Aryan Rajpal <rajpalaryan8@gmail.com>
2026-02-28 16:28:52 -0600

    Add app shell layout with navigation bar

--- app/opportunities/page.tsx
+++ app/opportunities/page.tsx
@@ -1,7 +1,7 @@
 export default function Opportunities() {
   return (
     <main>
-      <h1>Post Opportunity</h1>
+      <h1 className="text-3xl font-semibold">Post Opportunity</h1>
      </main>
   )
 }
